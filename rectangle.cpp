9994965bfeb02b2388a4025de32591135c5dd4b5
taphoclaptrinh <89983321+taphoclaptrinh@users.noreply.github.com>
2025-08-27 13:11:33 +0700

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,24 +1,46 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 class Rectangle {
-    public:
-        int height;
-        int weight;
+private:
+    int height;
+    int width;
 
-        Rectangle(const int& height, const int& weight) {
-            this->height = height;
-            this->weight = weight;
-        }
+public:
+    // Constructor
+    Rectangle(int h, int w) {
+        if (h > 0) height = h; else height = 0;
+        if (w > 0) width = w; else width = 0;
+    }
 
-        int calculateArea() {
-            return this->height * this->weight;
-        }
+    // Setter
+    void setHeight(int h) {
+        if (h > 0) height = h;
+    }
+
+    void setWidth(int w) {
+        if (w > 0) width = w;
+    }
+
+    // Getter
+    int getHeight() const {
+        return height;
+    }
+
+    int getWidth() const {
+        return width;
+    }
+
+    // Tính diện tích
+    int calculateArea() const {
+        return height * width;
+    }
 };
 
 int main() {
-    Rectangle rectangle1(5, 10); // Tạo một hình chữ nhật với chiều cao 5 và chiều rộng 10
+    Rectangle rectangle1(5, 10);
 
-    cout << "Area: " << rectangle1.calculateArea() << endl; // Tính và in diện tích
+    cout << "Area: " << rectangle1.calculateArea() << endl;
+
     return 0;
 }
